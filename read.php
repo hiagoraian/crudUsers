6f7a1b7aa5c9787088c6437bac6a8cb3bc02f0bd
hiagoraian <hiagogoncalves@proton.me>
2023-02-26 11:42:07 -0300

    Listando usuários

--- read.php
+++ read.php
@@ -1,1 +1,34 @@
 <h1>List Users</h1>
+<?php 
+    $sql = "SELECT * FROM users";
+    $res = $conn->query($sql);
+    $qtd = $res->num_rows;
+
+    if ($qtd > 0){
+
+        echo "<table class = 'table table-hover table-striped table-bordered'>";
+        echo "<tr>";
+            echo "<th>#</th>";
+            echo "<th>#Name</th>";
+            echo "<th>E-mail</th>";
+            echo "<th>Date of birth</th>";
+            echo "<th>Actions</th>";
+        echo "</tr>";
+
+        while($row = $res->fetch_object()){
+        echo "<tr>";
+            echo "<td>".$row->id."</td>";
+            echo "<td>".$row->name."</td>";
+            echo "<td>".$row->email."</td>";
+            echo "<td>".$row->date."</td>";
+            echo"<td>
+                <button class='btn btn-success'>Edit</button>
+                <button class='btn btn-danger'>Delete</button>
+                </td>";
+        echo "</tr>";
+        }}
+        else{
+            echo "<p> class = 'alert alert-danger'> Did not find results! </p>";
+
+        }
+?>
--- saveUsers.php
+++ saveUsers.php
@@ -9,6 +9,15 @@
             $sql = "INSERT INTO users (name,email, password, date) VALUES ('{$name}','{$email}','{$password}','{$date}')";
 
             $res = $conn->query($sql);
+
+            if($res == true){
+                echo "<script>alert('registration done.');</script>";
+                echo "<script> location.href='?page=read';</script>";
+            } else{
+                echo "<script>alert('unregistered registration.');</script>";
+                echo "<script> location.href='?page=read';</script>"; 
+            }
+
             break;
         
         case 'edit':
